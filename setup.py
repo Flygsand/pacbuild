5d95d070ab6e34be9932285259ba9f14c7aa9e04
Jason Chu <jchu@xentac.net>
2005-12-23 16:24:25 +0100

    Add more scripts to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-DATAFILES = [('/usr/share/waka', ['waka/functions', 'waka/build', 'waka/quickinst']), ('/etc', ['waka/mkchroot.conf'])]
+DATAFILES = [('/usr/share/waka', ['waka/functions', 'waka/build', 'waka/quickinst']), ('/etc', ['waka/mkchroot.conf', 'strawberryConfig.py', 'appleConfig.py'])]
 
 setup(name="pacbuild",
       version="0.1",
@@ -9,5 +9,5 @@ setup(name="pacbuild",
       author="Jason Chu",
       author_email="jason@archlinux.org",
       packages=["pacbuild"],
-      scripts=["cherry.py", "strawberry.py", "waka/mkchroot"],
+      scripts=["apple.py", "strawberry.py", "waka/mkchroot", "getBuild.py", "queuepackage.sh", "uploadPkgbuild.py"],
       data_files=DATAFILES)

4c6d0461c407a02e93e9f22a6be954ba027ae997
Simo Leone <simo@archlinux.org>
2005-12-24 09:26:18 +0100

    Apple status script

---
+++ status.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# 
+# status - Apple status cgi script
+# Copyright (C) 2005 Simo Leone <simo@archlinux.org>
+# 
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# 
+
+import sys
+sys.path.append('/etc')
+import appleConfig
+from pacbuild.apple import connect, package
+import cgi
+
+connect(appleConfig.database)
+
+def job_list():
+	print '''Content-type: text/html
+
+<html>
+<head>
+<title>Apple Status</title>
+</head>
+<body>
+<table cellpadding='5px'>
+<tr>
+	<th>Package</th><th>Status</th><th>Log</th><th>Package</th>
+</tr>
+'''
+	
+	for i in package.Package.select():
+		print "<tr>"
+		print "<td>%s-%s-%s</td><td>%s</td>"%(i.name,i.pkgver,i.pkgrel,i.status)
+		print "<td><a href='?action=log&id=%s'>Log</a></td>"%(i.id)
+		print "<td><a href='?action=pkg&id=%s'>%s-%s-%s.pkg.tar.gz</a></td>"%(i.id,i.name,i.pkgver,i.pkgrel)
+		print "</tr>"
+	print "</table></body></html>"
+
+def pkg_log(id=0):
+	print "Content-type: text/plain\n"
+	print package.Package.get(id).log
+
+def pkg_file(id=0):
+	pkg = package.Package.get(id)
+	print "Content-type: application/x-compressed;"
+	print "Content-Disposition: attachment; filename=\""+pkg.name+"-"+pkg.pkgver+"-"+pkg.pkgrel+".pkg.tar.gz\"\n"
+	sys.stdout.write(pkg.binary)
+	
+def main():
+	form = cgi.FieldStorage()
+	if (form.has_key("action") and form.has_key("id")):
+		if (form["action"].value == "log"):
+			pkg_log(form["id"].value)
+		elif (form["action"].value == "pkg"):
+			pkg_file(form["id"].value)
+		else:
+			job_list()
+	else:
+		job_list()
+
+main()

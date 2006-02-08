57baf74de89ae925abda4ecaf5005e3653faae47
Jason Chu <jchu@xentac.net>
2006-02-08 19:44:33 +0100

    Add -d option to strawberry

--- strawberry.py
+++ strawberry.py
@@ -35,6 +35,62 @@ defaultConfig = "/etc/strawberryConfig.py"
 #import strawberryConfig
 strawberryConfig = {}
 
+UMASK = 0
+
+WORKDIR = "/"
+
+MAXFD = 1024
+
+if (hasattr(os, "devnull")):
+   REDIRECT_TO = os.devnull
+else:
+   REDIRECT_TO = "/dev/null"
+
+def createDaemon():
+	"""Detach a process from the controlling terminal and run it in the
+	background as a daemon.
+	"""
+
+	try:
+		pid = os.fork()
+	except OSError, e:
+		raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+	if (pid == 0):	# The first child.
+		os.setsid()
+
+		try:
+			pid = os.fork()	# Fork a second child.
+		except OSError, e:
+			raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+		if (pid == 0):	# The second child.
+			os.chdir(WORKDIR)
+			os.umask(UMASK)
+		else:
+			os._exit(0)	# Exit parent (the first child) of the second child.
+	else:
+		os._exit(0)	# Exit parent of the first child.
+
+	import resource		# Resource usage information.
+	maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+	if (maxfd == resource.RLIM_INFINITY):
+		maxfd = MAXFD
+  
+	# Iterate through and close all file descriptors.
+	for fd in range(0, maxfd):
+		try:
+			os.close(fd)
+		except OSError:	# ERROR, fd wasn't open to begin with (ignored)
+			pass
+
+	os.open(REDIRECT_TO, os.O_RDWR)	# standard input (0)
+
+	os.dup2(0, 1)			# standard output (1)
+	os.dup2(0, 2)			# standard error (2)
+
+	return(0)
+
 class Build(SQLObject):
 	cherryId = IntCol()
 	sourceFilename = StringCol()
@@ -126,6 +182,7 @@ def sendBuild(build, binary, log):
 def usage():
 	print "usage: strawberry.py [-c <config>]"
 	print "       -c <config>     : use a different config than the default (%s)" % defaultConfig
+	print "       -d              : run as a daemon"
 	sys.exit(2)
 
 def _main(argv=None):
@@ -133,7 +190,7 @@ def _main(argv=None):
 		argv = sys.argv
 
 	try:
-		optlist, args = getopt.getopt(argv[1:], "c:")
+		optlist, args = getopt.getopt(argv[1:], "c:d")
 	except getopt.GetoptError:
 		usage()
 
@@ -142,6 +199,8 @@ def _main(argv=None):
 		if i == '-c':
 			if os.path.isfile(k):
 				configPath = k
+		if i == "-d":
+			createDaemon()
 
 	execfile(configPath, strawberryConfig, strawberryConfig)
 

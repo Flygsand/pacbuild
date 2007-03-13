f64620f4387ed11c49cdf348b4959e01b0ec15e7
Jason Chu <jchu@xentac.net>
2007-03-12 22:09:34 -0700

    Update setup.py version to 0.4 (oops)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 DATAFILES = [('/usr/share/waka', ['waka/functions', 'waka/build', 'waka/quickinst']), ('/etc', ['waka/mkchroot.conf', 'strawberryConfig.py', 'appleConfig.py']), ('/usr/share/pacbuild', ['peach.py'])]
 
 setup(name="pacbuild",
-      version="0.1",
+      version="0.4",
       description="Automated package builder",
       author="Jason Chu",
       author_email="jason@archlinux.org",
